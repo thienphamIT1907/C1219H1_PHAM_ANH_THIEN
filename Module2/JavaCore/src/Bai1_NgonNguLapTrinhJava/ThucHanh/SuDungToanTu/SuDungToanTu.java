95dfabf912a7650b7bd8cdd621f8376d0e1e70b2
Hajiiwan <phamanhthien1907@gmail.com>
2020-02-12 00:41:18 +0700

    Hoan thanh bai thuc hanh: Su dung toan tu

---
+++ Module2/JavaCore/src/Bai1_NgonNguLapTrinhJava/ThucHanh/SuDungToanTu/SuDungToanTu.java
@@ -0,0 +1,20 @@
+package Bai1_NgonNguLapTrinhJava.ThucHanh.SuDungToanTu;
+
+import java.util.Scanner;
+
+public class SuDungToanTu {
+    public static void main(String[] args) {
+        float width, height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+
+        float area = width * height;
+        System.out.println("Area is: " + area);
+    }
+}
