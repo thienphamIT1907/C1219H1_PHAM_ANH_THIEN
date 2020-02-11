adf05778c8127b34189507385887c0971175ae9c
thienphamIT1907 <phamanhthien1907@gmail.com>
2020-02-11 17:01:14 +0700

    Hoan thanh bai thuc hanh 2: Hien thi thoi gian he thong

---
+++ Module2/JavaCore/src/Bai1_NgonNguLapTrinhJava/ThucHanh/HienThiThoiGianHeThong/HienThiThoiGianHeThong.java
@@ -0,0 +1,10 @@
+package Bai1_NgonNguLapTrinhJava.ThucHanh.HienThiThoiGianHeThong;
+
+import java.util.Date;
+
+public class HienThiThoiGianHeThong {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: "  + now);
+    }
+}
