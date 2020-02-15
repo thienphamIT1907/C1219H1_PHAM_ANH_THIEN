3f5623b62e16a077b692caa54c574ca7bca8dc71
hajiiwan <phamanhthien1907@gmail.com>
2020-02-15 14:41:14 +0700

    Hoan thanh bai thuc hanh: Tim gia tri nho nhat trong mang - Su dung phuong thuc

---
+++ Module2/JavaCore/src/Bai2_MangVaPhuongThucTrongJava/ThucHanh/TimGiaTriNhoNhatTrongMang_PhuongThuc/TimGiaTriNhoNhatTrongMang_PhuongThuc.java
@@ -0,0 +1,23 @@
+package Bai2_MangVaPhuongThucTrongJava.ThucHanh.TimGiaTriNhoNhatTrongMang_PhuongThuc;
+
+public class TimGiaTriNhoNhatTrongMang_PhuongThuc {
+    public static void main(String[] args) {
+        int []arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+    public static int minValue(int []array){
+
+        int min = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++){
+            if(array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
