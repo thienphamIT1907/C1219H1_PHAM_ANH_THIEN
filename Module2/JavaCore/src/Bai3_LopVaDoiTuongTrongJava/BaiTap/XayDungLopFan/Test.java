271dd8c0790ece55019cb2d8323278a79dd27ff7
Hajiwann <phamanhthien1907@gmail.com>
2020-02-22 20:00:00 +0700

    Hoan thanh bai tap: Xay dung lop Fan

---
+++ Module2/JavaCore/src/Bai3_LopVaDoiTuongTrongJava/BaiTap/XayDungLopFan/Test.java
@@ -0,0 +1,11 @@
+package Bai3_LopVaDoiTuongTrongJava.BaiTap.XayDungLopFan;
+
+public class Test {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3,true, 10, "yellow");
+        Fan fan2 = new Fan(2,false,5,"blue");
+
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+    }
+}
