ad939853bac14bd1887b7b0c1482a834c35a24c3
Thien Pham <phamanhthien1907@gmail.com>
2020-04-23 14:39:45 +0700

    Hoan thanh bai thuc hanh: Ung dung quan ly khach hang - Thymeleaf

---
+++ Module2/JavaWeb/Views_and_Thymeleaf/ThucHanh/Bai1_SuDungThymeleafChoUngDungQuanLyKhachHang/src/main/java/com/example/thuchanh/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.example.thuchanh.service;
+
+import com.example.thuchanh.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
---
+++ Module2/JavaWeb/Views_and_Thymeleaf/ThucHanh/Bai1_SuDungThymeleafChoUngDungQuanLyKhachHang/src/test/java/com/example/thuchanh/ThuchanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thuchanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThuchanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
