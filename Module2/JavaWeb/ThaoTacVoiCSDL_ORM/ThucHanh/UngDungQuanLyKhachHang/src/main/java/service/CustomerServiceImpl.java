4a72eb81b141e546d6a3edf4da46bd2b1472a70e
Thien Pham <phamanhthien1907@gmail.com>
2020-04-24 18:18:27 +0700

    Hoan thanh bai thuc hanh: Ung dung quan ly khach hang

---
+++ Module2/JavaWeb/ThaoTacVoiCSDL_ORM/ThucHanh/UngDungQuanLyKhachHang/src/main/java/service/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+package service;
+
+import model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import repository.CustomerRepository;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.remove(id);
+    }
+}
