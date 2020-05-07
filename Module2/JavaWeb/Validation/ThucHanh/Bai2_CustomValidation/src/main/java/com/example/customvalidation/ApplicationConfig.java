365709c64650cc0c2298a77127545692d824bc6f
Thien Pham <phamanhthien1907@gmail.com>
2020-05-07 14:48:51 +0700

    Hoan thanh bai thuc hanh: Custom validation

---
+++ Module2/JavaWeb/Validation/ThucHanh/Bai2_CustomValidation/src/main/java/com/example/customvalidation/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package com.example.customvalidation;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.example.customvalidation")
+public class ApplicationConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    }
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("ValidationMessages");
+        return messageSource;
+    }
+}
