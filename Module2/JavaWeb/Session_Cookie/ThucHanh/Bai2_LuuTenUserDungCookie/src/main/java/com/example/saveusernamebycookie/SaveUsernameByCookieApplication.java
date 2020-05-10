df21501ff484ca720e76d9af2ef2588a8880c721
Thien Pham <phamanhthien1907@gmail.com>
2020-05-10 13:26:03 +0700

    Hoan thanh bai thuc hanh: Luu ten nguoi dung su dung cookie

---
+++ Module2/JavaWeb/Session_Cookie/ThucHanh/Bai2_LuuTenUserDungCookie/src/main/java/com/example/saveusernamebycookie/SaveUsernameByCookieApplication.java
@@ -0,0 +1,13 @@
+package com.example.saveusernamebycookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveUsernameByCookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveUsernameByCookieApplication.class, args);
+    }
+
+}
---
+++ Module2/JavaWeb/Session_Cookie/ThucHanh/Bai2_LuuTenUserDungCookie/src/test/java/com/example/saveusernamebycookie/SaveUsernameByCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saveusernamebycookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveUsernameByCookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
